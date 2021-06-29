88b41ea5ec1fd2d6c252757b5abdfd320c94367b
josephW1805 <joseph.weng.1805@gmail.com>
2021-06-29 16:52:29 -0400

    create category and list categories

--- client/src/pages/admin/category/CategoryCreate.js
+++ client/src/pages/admin/category/CategoryCreate.js
@@ -1,14 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+
+import {
+  createCategory,
+  getCategories,
+  removeCategory,
+} from "../../../functions/category";
+import Spinner from "../../../components/Spinner";
 
 const CategoryCreate = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = () =>
+    getCategories().then((c) => setCategories(c.data));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    createCategory({ name }, user.token)
+      .then((res) => {
+        setLoading(false);
+        setName("");
+        toast.success(`"${res.data.name}" is created`);
+      })
+      .catch((err) => {
+        setLoading(false);
+        if (err.response.status === 400) toast.error(err.response.data);
+      });
+  };
+
+  const categoryForm = () => (
+    <form onSubmit={handleSubmit}>
+      <div className="form-group">
+        <label>Name</label>
+        <input
+          type="text"
+          className="form-control"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+          autoFocus
+          required
+        />
+        <br />
+        <button className="btn btn-outline-primary">Save</button>
+      </div>
+    </form>
+  );
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col">Category create page</div>
+        <div className="col">
+          <h4>Create Category</h4>
+          {categoryForm()}
+          {loading && <Spinner />}
+          {JSON.stringify(categories)}
+        </div>
       </div>
     </div>
   );
