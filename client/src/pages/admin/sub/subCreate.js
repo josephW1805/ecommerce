88d952af0ab7e82c63219c0201c7f56dc8d142bc
josephW1805 <joseph.weng.1805@gmail.com>
2021-06-30 13:37:06 -0400

    Create in sub category

--- client/src/pages/admin/sub/subCreate.js
+++ client/src/pages/admin/sub/subCreate.js
@@ -1,15 +1,117 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getCategories } from "../../../functions/category";
+import { createSub, getSub, removeSub } from "../../../functions/sub";
+import Spinner from "../../../components/Spinner";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import CategoryForm from "../../../components/forms/CategoryForm";
+import LocalSearch from "../../../components/forms/LocalSearch";
 
 const SubCreate = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+
+  const [name, setName] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const [category, setCategory] = useState("");
+  const [keyword, setKeyword] = useState("");
+
+  useEffect(() => {
+    loadCategories();
+  }, []);
+
+  const loadCategories = () =>
+    getCategories().then((c) => setCategories(c.data));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    createSub({ name, parent: category }, user.token)
+      .then((res) => {
+        setLoading(false);
+        setName("");
+        toast.success(`"${res.data.name}" is created`);
+      })
+      .catch((err) => {
+        setLoading(false);
+        if (err.response.status === 400) toast.error(err.response.data);
+      });
+  };
+
+  const handleRemove = async (slug) => {
+    if (window.confirm("Are you sure you want to Delete?")) {
+      setLoading(true);
+      removeSub(slug, user.token)
+        .then((res) => {
+          setLoading(false);
+          toast.error(`"${res.data.name}" is deleted`);
+        })
+        .catch((err) => {
+          if (err.response.status === 400) {
+            setLoading(false);
+            toast.error(err.response.data);
+          }
+        });
+    }
+  };
+
+  const searched = (keyword) => (c) => c.name.toLowerCase().includes(keyword);
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
           <AdminNav />
         </div>
+        <div className="col">
+          <h4>Create sub Category</h4>
+          <div className="form-group">
+            <label>Parent Category</label>
+            <select
+              name="category"
+              className="form-control"
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option disabled selected>
+                Please select
+              </option>
+              {categories.length > 0 &&
+                categories.map((c) => (
+                  <option key={c._id} value={c._id}>
+                    {c.name}
+                  </option>
+                ))}
+            </select>
+          </div>
 
-        <div className="col-md-10">sub create page</div>
+          {JSON.stringify(category)}
+          <CategoryForm
+            handleSubmit={handleSubmit}
+            name={name}
+            setName={setName}
+          />
+          <LocalSearch keyword={keyword} setKeyword={setKeyword} />
+          {loading && <Spinner />}
+          {/* {categories.filter(searched(keyword)).map((c) => (
+            <div className="alert alert-primary" key={c._id}>
+              {c.name}
+              <span
+                onClick={() => handleRemove(c.slug)}
+                className="btn btn-sm float-right"
+              >
+                <DeleteOutlined className="text-danger" />
+              </span>
+              <Link to={`/admin/category/${c.slug}`}>
+                <span className="btn btn-sm float-right">
+                  <EditOutlined className="text-warning" />
+                </span>
+              </Link>
+            </div>
+          ))} */}
+        </div>
       </div>
     </div>
   );
