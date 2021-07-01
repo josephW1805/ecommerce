4d4f80a9bd2d4a666570a1cc5643de89180b08a7
josephW1805 <joseph.weng.1805@gmail.com>
2021-07-01 18:12:42 -0400

    Create product with sub categories

--- client/src/pages/admin/product/productCreate.js
+++ client/src/pages/admin/product/productCreate.js
@@ -55,11 +55,12 @@ const ProductCreate = () => {
   const handleCategoryChange = (e) => {
     e.preventDefault();
     console.log("CLICKED CATEGORY", e.target.value);
-    setValues({ ...values, category: e.target.value });
+    setValues({ ...values, subs: [], category: e.target.value });
     getCategorySubs(e.target.value).then((res) => {
       console.log("SUB OPTIONS ON CATEGORY CLICKED", res);
       setSubOptions(res.data);
     });
+    setShowSub(true);
   };
 
   return (
