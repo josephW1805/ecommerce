3db9057a3c94f584811e03e3d77c317b2df34ede
josephW1805 <joseph.weng.1805@gmail.com>
2021-07-02 18:25:43 -0400

    Get product on update page

--- client/src/pages/admin/product/ProductUpdate.js
+++ client/src/pages/admin/product/ProductUpdate.js
@@ -2,16 +2,43 @@ import React, { useState, useEffect } from "react";
 import AdminNav from "../../../components/nav/AdminNav";
 import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
-import { createProduct } from "../../../functions/product";
-import ProductCreateForm from "../../../components/forms/ProductCreateForm";
+import { getProduct } from "../../../functions/product";
 import { getCategories, getCategorySubs } from "../../../functions/category";
 import FileUpload from "../../../components/forms/FileUpload";
 import Spinner from "../../../components/Spinner";
 
+const initialState = {
+  title: "",
+  description: "",
+  price: "",
+  categories: [],
+  category: "",
+  subs: [],
+  shipping: "",
+  quantity: "",
+  images: [],
+  colors: ["Black", "Brown", "Silver", "White", "Blue"],
+  brands: ["Apple", "Samsung", "Microsoft", "Lenovo", "ASUS"],
+  color: "",
+  brand: "",
+};
+
 const ProductUpdate = ({ match }) => {
+  // state
+  const [values, setValues] = useState(initialState);
   const { user } = useSelector((state) => ({ ...state }));
   // router
   const { slug } = match.params;
+
+  useEffect(() => {
+    loadProduct();
+  }, []);
+
+  const loadProduct = () => {
+    getProduct(slug).then((p) => {
+      setValues({ ...values, ...p.data });
+    });
+  };
 
   return (
     <div className="container-fluid">
@@ -21,7 +48,7 @@ const ProductUpdate = ({ match }) => {
         </div>
         <div className="col-md-10">
           <h4>Update Product</h4>
-          {JSON.stringify(slug)}
+          {JSON.stringify(values)}
         </div>
       </div>
     </div>
