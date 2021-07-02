f2478a38c4a7f6e343eb10b03d7607ca6594435f
josephW1805 <joseph.weng.1805@gmail.com>
2021-07-02 13:17:11 -0400

    Fetch all products in Admin Dashboard

--- client/src/pages/admin/AdminDashboard.js
+++ client/src/pages/admin/AdminDashboard.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AdminNav from "../../components/nav/AdminNav";
+import { getProductByCount } from "../../functions/product";
+import Spinner from "../../components/Spinner";
 
 const AdminDashboard = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loadAllProducts();
+  }, []);
+
+  const loadAllProducts = () => {
+    setLoading(true);
+    getProductByCount(100)
+      .then((res) => {
+        setProducts(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-md-2">
           <AdminNav />
         </div>
-        <div className="col">admin dashboard page</div>
+        <h4>All Products</h4>
+        {loading && <Spinner />}
+        <div className="col">{JSON.stringify(products)}</div>
       </div>
     </div>
   );
