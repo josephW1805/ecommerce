0201f16b1a5be5355dcd1476060323c49f0ef4db
josephW1805 <joseph.weng.1805@gmail.com>
2021-07-02 09:55:32 -0400

    FIle resizer

--- client/src/components/forms/FileUpload.js
+++ client/src/components/forms/FileUpload.js
@@ -1,10 +1,32 @@
 import React from "react";
+import Resizer from "react-image-file-resizer";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const FileUpload = () => {
+  const { user } = useSelector((state) => ({ ...state }));
+
   const fileUploadAndResize = (e) => {
-    // resize, send back to server to upload to cloudinary
+    // resize
+    let files = e.target.files;
+    if (files) {
+      for (let i = 0; i < files.length; i++) {
+        Resizer.imageFileResizer(
+          files[i],
+          720,
+          720,
+          "JPEG",
+          100,
+          0,
+          (uri) => {
+            console.log(uri);
+          },
+          "base64"
+        );
+      }
+    }
+    // send back to server to upload to cloudinary
     // set url to image[] in the parent component - ProductCreate
-    
   };
   return (
     <div className="row">
