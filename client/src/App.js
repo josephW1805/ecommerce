c785fc366a0b84e8377af7220468d27ea277c08c
josephW1805 <joseph.weng.1805@gmail.com>
2021-07-04 16:49:21 -0400

    View Products Page

--- client/src/App.js
+++ client/src/App.js
@@ -22,6 +22,7 @@ import SubUpdate from "./pages/admin/sub/subUpdate";
 import ProductCreate from "./pages/admin/product/ProductCreate";
 import AllProducts from "./pages/admin/product/AllProducts";
 import ProductUpdate from "./pages/admin/product/ProductUpdate";
+import Product from "./pages/Product";
 
 import { auth } from "./firebase";
 import { useDispatch } from "react-redux";
@@ -65,6 +66,7 @@ const App = () => {
         <Route exact path="/register" component={Register} />
         <Route exact path="/register/complete" component={RegisterComplete} />
         <Route exact path="/forgot/password" component={ForgotPassword} />
+        <Route exact path="/product/:slug" component={Product} />
         <UserRoute exact path="/user/history" component={History} />
         <UserRoute exact path="/user/password" component={Password} />
         <UserRoute exact path="/user/wishlist" component={Wishlist} />
