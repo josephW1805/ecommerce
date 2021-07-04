5d379c5a861235689185ad2128fc6b74063908bc
josephW1805 <joseph.weng.1805@gmail.com>
2021-07-04 14:45:19 -0400

    New Arrivals

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,43 +1,19 @@
-import React, { useEffect, useState } from "react";
-import { getProductByCount } from "../functions/product";
-import ProductCard from "../components/cards/ProductCard";
+import React from "react";
 import Jumbotron from "../components/cards/Jumbotron";
-import LoadingCard from "../components/cards/LoadingCard";
+import NewArrivals from "../components/home/NewArrivals";
 
 const Home = () => {
-  const [products, setProducts] = useState([]);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    loadAllProducts();
-  }, []);
-
-  const loadAllProducts = () => {
-    setLoading(true);
-    getProductByCount(3).then((res) => {
-      setProducts(res.data);
-      setLoading(false);
-    });
-  };
-
   return (
     <>
       <div className="jumbotron text-info h1 font-weight-bold text-center">
         <Jumbotron text={["Latest Products", "New Arrivals", "Best Sellers"]} />
       </div>
-      <div className="container">
-        {loading ? (
-          <LoadingCard count={3} />
-        ) : (
-          <div className="row">
-            {products.map((product) => (
-              <div key={product._id} className="col-md-4">
-                <ProductCard product={product} />
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+
+      <h4 className="text-center p-3 mt-5 mb-5 display-4 jumbotron">
+        New Arrivals
+      </h4>
+      <NewArrivals />
+      <br />
     </>
   );
 };
