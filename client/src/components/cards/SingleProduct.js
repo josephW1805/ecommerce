7a6fac06cd5b9af88fb0ac97be0ed6be551e6340
josephW1805 <joseph.weng.1805@gmail.com>
2021-07-05 12:14:01 -0400

    Tabs

--- client/src/components/cards/SingleProduct.js
+++ client/src/components/cards/SingleProduct.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card } from "antd";
+import { Card, Tabs } from "antd";
 import { Link } from "react-router-dom";
 import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
@@ -7,8 +7,10 @@ import { Carousel } from "react-responsive-carousel";
 import Laptop from "../../images/laptop.png";
 import ProductListItems from "./ProductListItems";
 
+const { TabPane } = Tabs;
+
 const SingleProduct = ({ product }) => {
-  const { title, images } = product;
+  const { title, images, description } = product;
 
   return (
     <>
@@ -23,6 +25,14 @@ const SingleProduct = ({ product }) => {
             cover={<img alt="" src={Laptop} className="mb-3 card-image" />}
           ></Card>
         )}
+        <Tabs type="card">
+          <TabPane tab="Description" key="1">
+            {description && description}
+          </TabPane>
+          <TabPane tab="More" key="2">
+            Call us on xxxx xxx xxx to learn more about this product
+          </TabPane>
+        </Tabs>
       </div>
       <div className="col-md-5">
         <h1 className="p-3">{title}</h1>
--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -22,7 +22,11 @@ const Product = ({ match }) => {
         <SingleProduct product={product} />
       </div>
       <div className="row">
-        <div>Related products</div>
+        <div className="col text-center pt-5 pb-5">
+          <hr />
+          <h4>Related products</h4>
+          <hr />
+        </div>
       </div>
     </div>
   );
