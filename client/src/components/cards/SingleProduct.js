8b447cbae740afc0e2e6660058a11549a877646a
josephW1805 <joseph.weng.1805@gmail.com>
2021-07-05 10:07:42 -0400

    SIngle product Component

---
+++ client/src/components/cards/SingleProduct.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Card } from "antd";
+import { Link } from "react-router-dom";
+import { HeartOutlined, ShoppingCartOutlined } from "@ant-design/icons";
+
+const { Meta } = Card;
+
+const SingleProduct = ({ product }) => {
+  const { title, description, images, slug } = product;
+  return (
+    <>
+      <div className="col-md-7">image carousel</div>
+      <div className="col-md-5">
+        <Card
+          actions={[
+            <>
+              <ShoppingCartOutlined className="text-success" />
+              <br />
+              Add to Cart
+            </>,
+            <Link to="/">
+              <HeartOutlined className="text-danger" />
+              <br />
+              Add to Wishlist
+            </Link>,
+          ]}
+        >
+          <Meta title={title} description={description} />
+          <p>
+            price/category/subs/shipping/color/brand/quantity available/sold
+          </p>
+        </Card>
+      </div>
+    </>
+  );
+};
+
+export default SingleProduct;
--- client/src/pages/Product.js
+++ client/src/pages/Product.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getProduct } from "../functions/product";
+import SingleProduct from "../components/cards/SingleProduct";
 
 const Product = ({ match }) => {
   const [product, setProduct] = useState({});
@@ -15,7 +16,16 @@ const Product = ({ match }) => {
       setProduct(res.data);
     });
 
-  return <>{JSON.stringify(product)}</>;
+  return (
+    <div className="container-fluid">
+      <div className="row pt-4">
+        <SingleProduct product={product} />
+      </div>
+      <div className="row">
+        <div>Related products</div>
+      </div>
+    </div>
+  );
 };
 
 export default Product;
