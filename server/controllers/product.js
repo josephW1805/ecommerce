3a466a7b7339b97191ff6fff0fb22c428a4bb110
josephW1805 <joseph.weng.1805@gmail.com>
2021-07-06 17:06:36 -0400

    Search filter backend

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -139,3 +139,24 @@ exports.listRelated = async (req, res) => {
 
   res.json(related);
 };
+
+// SEARCH / FILTER
+
+exports.searchFilters = async (req, res) => {
+  const { query } = req.body;
+
+  const handleQuery = async (req, res, query) => {
+    const products = await Product.find({ $text: { $search: query } })
+      .populate("category", "_id name")
+      .populate("subs", "_id name")
+      .populate("postedBy", "_id name")
+      .exec();
+
+    res.json(products);
+  };
+
+  if (query) {
+    console.log("query", query);
+    await handleQuery(req, res, query);
+  }
+};
