e45762ca83d88972b5b881418c4bf4c8c061c536
josephW1805 <joseph.weng.1805@gmail.com>
2021-07-08 16:31:18 -0400

    Side drawer component

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { Switch, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import SideDrawer from "./components/drawer/SideDrawer";
 
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
@@ -63,6 +64,7 @@ const App = () => {
   return (
     <>
       <Header />
+      <SideDrawer />
       <ToastContainer />
       <Switch>
         <Route exact path="/" component={Home} />
