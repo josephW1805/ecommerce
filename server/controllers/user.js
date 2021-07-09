5b0bacd7e2dba9cd1f19017e2c601816b88fb424
josephW1805 <joseph.weng.1805@gmail.com>
2021-07-09 16:09:03 -0400

    Get user cart from database

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -46,6 +46,17 @@ exports.userCart = async (req, res) => {
     orderedBy: user._id,
   }).save();
 
-  console.log("new cart", newCart);
+  console.log("new cart -->", newCart);
   res.json({ ok: true });
+};
+
+exports.getUserCart = async (req, res) => {
+  const user = await User.findOne({ email: req.params.email }).exec();
+
+  let cart = await Cart.findOne({ orderedBy: user._id })
+    .populate("products.product", "_id title price totalAfterDiscount")
+    .exec();
+
+  const { products, cartTotal, totalAfterDiscount } = cart;
+  res.json({ products, cartTotal, totalAfterDiscount });
 };
--- server/routes/user.js
+++ server/routes/user.js
@@ -5,8 +5,9 @@ const router = express.Router();
 // middleware
 const { authCheck } = require("../middleware/auth");
 // controller
-const { userCart } = require("../controllers/user");
+const { userCart, getUserCart } = require("../controllers/user");
 
-router.post("/cart", authCheck, userCart); // save cart
+router.post("/user/cart", authCheck, userCart); // save cart
+router.get("/user/cart", authCheck, getUserCart); // get cart
 
 module.exports = router;
