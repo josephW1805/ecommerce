463c43f072b9adfbfbcff10361a723511c08a0e0
josephW1805 <joseph.weng.1805@gmail.com>
2021-07-09 18:06:58 -0400

    Empty cart

--- client/src/pages/Checkout.js
+++ client/src/pages/Checkout.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { toast } from "react-toastify";
 import { getUserCart, emptyUserCart } from "../functions/user";
+import { Button } from "antd";
 
 const Checkout = ({ history }) => {
   const [products, setProducts] = useState([]);
@@ -64,31 +65,34 @@ const Checkout = ({ history }) => {
       <div className="col-md-6">
         <h4>Order Summary</h4>
         <hr />
-        <p>Products {products.length}</p>
+        <p> {products.length} products</p>
         <hr />
         {products.map((p, i) => (
           <div key={i}>
             <p>
-              {p.product.title} ({p.color}) = {p.product.price * p.count}
+              {p.product.title} ({p.color}) x {p.count} = $
+              {p.product.price * p.count}
             </p>
           </div>
         ))}
         <hr />
-        <p>Cart Total: {total}</p>
+        <p>Cart Total: ${total}</p>
 
         <div className="row">
           <div className="col-md-6">
-            <button className="btn btn-primary">Place Order</button>
+            <Button disabled={!products.length} className="btn btn-primary">
+              Place Order
+            </Button>
           </div>
 
           <div className="col-md-6">
-            <button
+            <Button
               disabled={!products.length}
               onClick={emptyCart}
               className="btn btn-primary"
             >
               Empty Cart
-            </button>
+            </Button>
           </div>
         </div>
       </div>
